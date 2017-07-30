4d61f35e8cfd4f2da001106a1a86ca5591d6a545
too-r <william@huddie.net>
2017-07-30 12:27:17 +0000

    Remove libs directory from gitignore

---
+++ libs/hole_list_allocator/src/lib.rs
@@ -0,0 +1,47 @@
+#![deny(warnings)]
+#![feature(alloc)]
+#![feature(allocator_api)]
+#![feature(custom_attribute)]
+#![feature(const_fn)]
+#![no_std]
+#![feature(global_allocator)]
+
+extern crate alloc;
+extern crate spin;
+extern crate linked_list_allocator;
+
+use alloc::heap::{Alloc, AllocErr, Layout};
+use spin::Mutex;
+use linked_list_allocator::Heap;
+
+pub const HEAP_START: usize = 0o_000_001_000_000_0000;
+pub const HEAP_SIZE: usize = 100 * 1024;
+
+static HEAP: Mutex<Option<Heap>> = Mutex::new(None);
+
+pub unsafe fn init(offset: usize, size: usize) {
+    *HEAP.lock() = Some(Heap::new(offset, size));
+}
+
+pub struct Allocator;
+
+unsafe impl<'a> Alloc for &'a Allocator {
+    unsafe fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
+        if let Some(ref mut heap) = *HEAP.lock() {
+            heap.allocate_first_fit(layout)
+        } else {
+            panic!("heap not initialized");
+        }
+    }
+
+    unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
+        if let Some(ref mut heap) = *HEAP.lock() {
+            heap.deallocate(ptr, layout)
+        } else {
+            panic!("heap not initialized");
+        }
+    }
+}
+
+#[global_allocator]
+static GLOBAL_ALLOC: Allocator = Allocator;
