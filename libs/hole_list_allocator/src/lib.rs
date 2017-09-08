294cbf33d0f8201daf4c63777b8e948d4a0787e8
too-r <william@huddie.net>
2017-09-08 10:44:08 +0000

    Add some comments in the allocator lib

--- libs/hole_list_allocator/src/lib.rs
+++ libs/hole_list_allocator/src/lib.rs
@@ -19,6 +19,7 @@ pub const HEAP_SIZE: usize = 100 * 1024;
 
 static HEAP: Mutex<Option<Heap>> = Mutex::new(None);
 
+//Create a heap.
 pub unsafe fn init(offset: usize, size: usize) {
     *HEAP.lock() = Some(Heap::new(offset, size));
 }
@@ -28,7 +29,7 @@ pub struct Allocator;
 unsafe impl<'a> Alloc for &'a Allocator {
     unsafe fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
         if let Some(ref mut heap) = *HEAP.lock() {
-            heap.allocate_first_fit(layout)
+            heap.allocate_first_fit(layout) //Allocate memory at the first available space big enough.
         } else {
             panic!("heap not initialized");
         }
@@ -36,12 +37,13 @@ unsafe impl<'a> Alloc for &'a Allocator {
 
     unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
         if let Some(ref mut heap) = *HEAP.lock() {
-            heap.deallocate(ptr, layout)
+            heap.deallocate(ptr, layout) //Deallocate the memory using a pointer to the allocated memory.
         } else {
             panic!("heap not initialized");
         }
     }
 }
 
+//Attribute tells Rust to use this as the default heap allocator.
 #[global_allocator]
 static GLOBAL_ALLOC: Allocator = Allocator;
