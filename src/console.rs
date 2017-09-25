623bc7dadee44257901a226222107639514c3a18
too-r <william@huddie.net>
2017-09-25 17:36:50 +0000

    Add a console abstraction around the VGA driver

---
+++ src/console.rs
@@ -0,0 +1,13 @@
+use core::fmt;
+use spin::Mutex;
+use vga;
+
+pub struct Console;
+
+impl fmt::Write for Console {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        try!(vga::SCREEN.lock().write_str(s));
+    }
+}
+
+pub static CONSOLE: Mutex<Console> = Mutex::new(Console);
