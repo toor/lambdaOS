63c0fa358d59b033d6b4e3cdf250e0aafd6e1811
too-r <william@huddie.net>
2017-10-04 20:05:42 +0000

    Fix the result returned in Console::write_str

--- src/console.rs
+++ src/console.rs
@@ -1,12 +1,14 @@
 use core::fmt;
 use spin::Mutex;
 use vga;
+use io::serial;
 
 pub struct Console;
 
 impl fmt::Write for Console {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        try!(vga::SCREEN.lock().write_str(s))
+        try!(vga::SCREEN.lock().write_str(s));
+        serial::COM1.lock().write_str(s)
     }
 }
 
