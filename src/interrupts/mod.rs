c6860bf6ae3a68d96badc843049593ae264f7cca
too-r <william@huddie.net>
2017-10-22 18:30:31 +0000

    Create static interface to the PICS and also call ChainedPics initialization function

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -8,6 +8,10 @@ use x86_64::structures::tss::TaskStateSegment;
 use x86_64::VirtualAddress;
 use memory::MemoryController;
 use handlers::IDT;
+use io;
+
+//Our interface to the PICS.
+pub static PICS: Mutex<io::ChainedPics> = Mutex::new(unsafe {io::ChainedPics::new(0x20, 0x28) }); 
 
 pub fn initialize() {
     //Load the GDT.
@@ -17,4 +21,7 @@ pub fn initialize() {
     let idt_ptr = new_idtp(&IDT);
     //Load IDT from this pointer.
     lidt(&idt_ptr);
+
+    //Remap the 8259 PIC.
+    PICS.lock().init();
 }
