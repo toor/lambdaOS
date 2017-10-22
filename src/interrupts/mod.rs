d6ff50065789ebe451057c1f7c1c7131379a53ed
too-r <william@huddie.net>
2017-10-22 18:11:30 +0000

    Write out init function in interrupts module

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -2,7 +2,19 @@ mod gdt;
 #[macro_use]
 mod handlers;
 use x86::bits64::irq::IdtEntry;
+use x86::shared::dtables::lidt;
+use x86::shared::dtables::DescriptorTablePointer::new_idtp;
 use x86_64::structures::tss::TaskStateSegment;
 use x86_64::VirtualAddress;
 use memory::MemoryController;
+use handlers::IDT;
 
+pub fn initialize() {
+    //Load the GDT.
+    gdt::Gdt::load();
+    
+    //Create a pointer to our Idt.
+    let idt_ptr = new_idtp(&IDT);
+    //Load IDT from this pointer.
+    lidt(&idt_ptr);
+}
