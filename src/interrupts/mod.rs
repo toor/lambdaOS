b9c41d7832fa52b45d9f480593b57be8df6a2a8f
too-r <william@huddie.net>
2017-10-22 16:12:20 +0000

    Add method to load the Idt

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -4,3 +4,19 @@ use x86_64::structures::tss::TaskStateSegment;
 use x86_64::VirtualAddress;
 use memory::MemoryController;
 
+pub struct Idt {
+    table: [IdtEntry; 256],
+}
+
+impl Idt {
+    pub fn load(&mut self) {
+        use x86_64::instructions::tables::{DescriptorTablePointer, lidt};
+
+        let ptr = DescriptorTablePointer {
+            base: self.table.as_ptr() as u64,
+            limit: (self.table.len() * size_of::<u64>() -1) as u16,
+        };
+        
+        unsafe { lidt(&ptr) };
+    }
+}
