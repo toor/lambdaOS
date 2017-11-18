342cfd4527da4385ddeac20cb0306344c586fb16
too-r <william@huddie.net>
2017-11-18 17:48:13 +0000

    Fix kalloc with ugly unwraps and stuff

--- src/libkernel/mod.rs
+++ src/libkernel/mod.rs
@@ -13,10 +13,14 @@ pub fn kalloc(size: usize) {
 
     if let Some(l) = layout {
         //Layout created successfully, allocate some memory on the heap with it.
-        if size > 100 * 1024 {
+        if size > (100 * 1024) {
             panic!("requested size is larger than the available heap memory");
         } else {
-            unsafe { *HEAP.lock().unwrap().alloc(l).unwrap() };
+            let mut heap = HEAP.lock();
+            let mut heap = heap.as_mut();
+            let mut heap = heap.unwrap();
+
+            unsafe { heap.alloc(l).unwrap() };
         }
     } else {
         panic!("Invalid layout");
