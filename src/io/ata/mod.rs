779d9c18c68ab31b5612df17f083e575ab7fb58d
too-r <william@huddie.net>
2017-11-21 11:06:26 +0000

    Make AtaDevice fields public, mostly create a new device under new(), and begin work on retrieving identity data

--- src/io/ata/mod.rs
+++ src/io/ata/mod.rs
@@ -24,29 +24,65 @@ pub struct CachedSector {
 }
 
 pub struct AtaDevice {
-    master: u8,
-    identify: [u8; 256],
-    data_port: Port<u16>,
-    error_port: Port<u16>,
-    sector_count_port: Port<u16>,
-    lba_low_port: Port<u16>,
-    lba_mid_port: Port<u16>,
-    lba_hi_port: Port<u16>,
-    device_port: Port<u16>,
-    command_port: Port<u16>,
-    control_port: Port<u16>,
-    exists: u8,
-    sector_count: u64,
-    bytes_per_sector: u16,
-    cache: &CachedSector,
+    //http://wiki.osdev.org/ATA_PIO_Mode#Registers
+    pub master: u8,
+    pub identify: [u8; 256],
+    pub data_port: Port<u16>,
+    pub error_port: Port<u16>,
+    pub sector_count_port: Port<u16>,
+    pub lba_low_port: Port<u16>,
+    pub lba_mid_port: Port<u16>,
+    pub lba_hi_port: Port<u16>,
+    pub device_port: Port<u16>,
+    pub command_port: Port<u16>,
+    pub control_port: Port<u16>,
+    pub exists: u8,
+    pub sector_count: u64,
+    pub bytes_per_sector: u16,
+    pub cache: &CachedSector,
 }
 
 impl AtaDevice {
-    pub fn new(&self) -> AtaDevice {
+    pub fn new(&self, port_base: u16, master: u8) -> AtaDevice {
         //Retrieve identity data.
-        let ata = AtaDevice {
-            master: 0,
-            identify: []
+        let mut dev = AtaDevice {
+            master: master,
+            identify: [0],
+            data_port: port_base,
+            error_port: Port::new(port_base + 0x01),
+            sector_count_port: Port::new(port_base + 0x02),
+            lba_low_port: Port::new(port_base + 0x03),
+            lba_mid_port: Port::new(port_base + 0x04),
+            lba_hi_port: Port::new(port_base + 0x05),
+            device_port: Port::new(port_base + 0x06),
+            command_port: Port::new(port_base + 0x07),
+            dev.control_port: Port::new(port_base + 0x206),
+            exists: 0,
+            bytes_per_sector: 512,
+            //TODO: Use kalloc to create some cache for the disk.
+        };
+
+        if dev.master == 1 {
+            dev.device_port.write(0xa0);
+        } else {
+            dev.device_port.write(0xb0);
+        }
+        
+        //Zero sec_count and lba ports.
+        dev.sector_count_port.write(0);
+        dev.lba_low_port.write(0);
+        dev.lba_mid_port.write(0);
+        dev.lba_hi_port.write(0);
+
+        if dev.command_port.read() == 0 {
+            dev.exists = 0;
+            println!("No device found");
+            return dev;
+        } else {
+            let timeout: u32 = 0;
+            while (dev.command_port.read() & 0b10000000) {
+                
+            }
         }
     }
 }
