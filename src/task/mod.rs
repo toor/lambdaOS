317f5e42737dac5e4ea413733eebe26302b03079
too-r <william@huddie.net>
2017-12-16 17:59:02 +0000

    Add global lazy scheduler static

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -9,6 +9,7 @@ use self::coop_sched as scheduler;
 
 pub use self::process::{Process, ProcessId, State};
 pub use self::proc_list::ProcessList;
+pub use self::scheduler::Scheduler;
 use core::result::Result;
 use alloc::string::String;
 
@@ -26,3 +27,7 @@ pub const MAX_PROCS: usize = usize::max_value() - 1;
 
 //TODO use max value of u16 as initial stack size.
 pub const INITIAL_STACK: usize = 1000;
+
+lazy_static! {
+    pub static ref SCHEDULER: Scheduler = Scheduler::new();
+}
