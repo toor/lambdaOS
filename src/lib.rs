c1525441fe42d4b4c5dbacd47897d11df0030d83
too-r <william@huddie.net>
2017-12-16 17:58:33 +0000

    Clean some stuff up, test scheduler

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,6 @@ extern crate once;
 extern crate bit_field;
 #[macro_use]
 extern crate lazy_static;
-
 extern crate hole_list_allocator as allocator;
 #[macro_use]
 extern crate alloc;
@@ -31,34 +30,26 @@ mod macros;
 pub mod memory;
 pub mod io;
 pub mod interrupts;
-mod debug;
 mod libkernel;
 mod task;
+mod syscall;
 
 use io::pic::ChainedPics;
 use spin::Mutex;
 use multiboot2::BootInformation;
-use libkernel::*;
-use core::cell::RefCell;
-use alloc::rc::Rc;
 
 //Constants and statics.
 pub const HEAP_START: usize = 0o_000_001_000_000_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 pub static PICS: Mutex<ChainedPics> = Mutex::new(unsafe { ChainedPics::new(0x20, 0x28) });
-pub static mut BOOT_INFO: Option<&BootInformation> = None;
 
 #[no_mangle]
 pub extern "C" fn kmain(multiboot_information_address: usize) {
-    // ATTENTION: we have a very small stack and no guard page
     io::vga::buffer::clear_screen();
     println!("Hello World{}", "!");
     
     //Load a multiboot BootInfo structure using the address passed in ebx.
     let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
-    
-    //Set up BOOT_INFO static so it can be used for printing debug info.
-    unsafe { BOOT_INFO = Some(boot_info) };
     
     //Safety stuff.
     enable_nxe_bit();
@@ -66,13 +57,12 @@ pub extern "C" fn kmain(multiboot_information_address: usize) {
 
     // set up guard page and map the heap pages
     let mut memory_controller = memory::init(boot_info);
-    let mut memc_lock: Rc<RefCell<memory::MemoryController>> = Rc::new(RefCell::new(memory_controller));
 
     //Clear interrupts
     unsafe { asm!("cli") };
     // initialize our IDT
     println!("Loading IDT.");
-    interrupts::init(&mut memc_lock.borrow_mut());
+    interrupts::init(&mut memory_controller);
 
     //Init PICS.
     unsafe { PICS.lock().init() };
@@ -82,9 +72,31 @@ pub extern "C" fn kmain(multiboot_information_address: usize) {
 
     println!("It did not crash!");
     
-    //Test kalloc
-    io::vga::buffer::clear_screen();
-    loop {}
+    use alloc::String;
+        
+    syscall::create(real_main, String::from("real_main"));
+    
+    let mut i = 0;
+
+    loop {
+        use task::Scheduling;
+
+        syscall::create(process_test, format!("test_process_{}", i));
+        println!("Running test process {}", i);
+
+        unsafe {
+            task::SCHEDULER.resched();
+            i += 1;
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn real_main() {
+    println!("In real main");
+}
+
+pub extern "C" fn process_test() {
 }
 
 fn enable_nxe_bit() {
