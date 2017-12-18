d770f5adb6690123a3a798bd1c262292d8546c0a
too-r <william@huddie.net>
2017-12-18 20:00:56 +0000

    Remove duplicate constants, remove ata driver.

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,21 +1,16 @@
 use core::marker::PhantomData;
 
-///Command to begin init of the PIC chip.
-const CMD_INIT: u8 = 0x11;
-
-///EOI command, that tells the PIC it can begin receiving other interrupts again.
-const CMD_END_OF_INTERRUPT: u8 = 0x20;
-
-///The PIC lives in ancient 8086 land.
-const MODE_8086: u8 = 0x01;
-
 #[macro_use]
+///Primary interface to I/O ports - special memory addresses on a different bus that we can use to
+///access I/O devices.
 pub mod cpuio;
+///Serial driver.
 pub mod serial;
+///Simple driver for the PS/2 keyboard.
 pub mod keyboard;
+///Advanced split interface to VGA buffer split between text management and actual buffer
+///addressing. 
 pub mod vga;
+///The 8259 PIC - Programmable Interrupt Controller. Contains data structures that we can use to
+///control the behaviour of this device.
 pub mod pic;
-//pub mod ata;
-use self::cpuio::Port;
-
-
