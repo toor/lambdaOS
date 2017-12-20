194bb0ee19903557358add6bca80d145cf04c114
too-r <william@huddie.net>
2017-12-20 18:21:30 +0000

    Update PIT driver, using new configuration data. Add new constants.

--- src/io/pit.rs
+++ src/io/pit.rs
@@ -5,6 +5,15 @@ const SELECT_CHAN_0: u8 = 0;
 const LOHI: u8 = 0x30;
 const DIVISOR: u16 = 2685;
 
+const PIT_A: u16 = 0x43;
+const PIT_CONTROL: u16 = 0x40;
+const PIT_MASK: u8 = 0xFF;
+const PIT_SCALE: u32 = 1193180;
+/// Select Mode 0, lobyte/hibyte operation, mode 3: square wave generator.
+/// Set BCD as 0 - 16-bit binary.
+const PIT_SET: u8 = 0x36;
+const SUBTICKS: u16 = 1000;
+
 pub struct Pit {
     pub control: Port<u8>,
     pub chan0: Port<u8>,
@@ -21,10 +30,18 @@ impl Pit {
 
 pub static PIT: Mutex<Pit> = Mutex::new(unsafe { Pit::new(0x43, 0x40) });
 
-pub unsafe fn init() {
-    PIT.lock().control.write(SELECT_CHAN_0 | LOHI | 5);
-    PIT.lock().chan0.write((DIVISOR & 0xFF) as u8);
-    PIT.lock().chan0.write((DIVISOR >> 8) as u8);
+pub fn init() {
+    let mut divisor: u32 = PIT_SCALE / SUBTICKS as u32;
+    
+    let config_1: u8 = (divisor & (PIT_MASK as u32)) as u8;
+    let config_2: u8 = ((divisor >> 8) & (PIT_MASK as u32)) as u8;
+    
+    // Setup using mode data.
+    PIT.lock().control.write(PIT_SET);
+    
+    // We are now free to configure the divisor via the Channel 0 port.
+    PIT.lock().chan0.write(config_1);
+    PIT.lock().chan0.write(config_2);
 
     println!("Using PIT controller");
 }
