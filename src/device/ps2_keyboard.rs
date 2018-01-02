959610510a41e740e0ecdebc9a74bed4ebb8dc1e
too-r <william@huddie.net>
2018-01-02 12:01:18 +0000

    Implement scancode sequences and important enums

---
+++ src/device/ps2_keyboard.rs
@@ -0,0 +1,115 @@
+use device::ps2_8042;
+
+#[derive(Debug)]
+struct KeyPair {
+    left: bool,
+    right: bool,
+}
+
+impl KeyPair {
+    const fn new() -> Self {
+        KeyPair { left: false, right: false}
+    }
+
+    fn is_pressed(&self) -> bool {
+        self.left || self.right
+    }
+}
+
+pub enum Modifiers {
+    AltLeft(bool),
+    AltRight(bool),
+    CapsLock,
+    ControlLeft(bool),
+    ControlRight(bool),
+    NumLock,
+    ScrollLock,
+    ShiftLeft(bool),
+    ShiftRight(bool),
+}
+
+struct ModifierState {
+    shift: KeyPair,
+    control: KeyPair,
+    alt: KeyPair,
+    caps_lock: bool,
+    num_lock: bool,
+    scroll_lock: bool,
+}
+
+impl ModifierState {
+    const fn new() -> Self {
+        ModifierState {
+            shift: KeyPair::new(),
+            control: KeyPair::new(),
+            alt: KeyPair::new(),
+            caps_lock: false,
+            num_lock: false,
+            scroll_lock: bool,
+        }
+    }
+
+    fn use_uppercase_letters(&self) -> bool {
+        self.shift.is_pressed() ^ self.caps_lock
+    }
+    
+    /// Apply modifiers to ascii and return updated ascii.
+    fn apply_to(&self, ascii: u8) -> u8 {
+        if b'a' <= ascii && ascii <= b'z' {
+            if self.use_uppercase_letters() {
+                return ascii - b'a' + b'A';
+            }
+        }
+
+        ascii
+    }
+    
+    /// Update modifier state.
+    fn update(&mut self, modifier: Modifiers) {
+        use self::Modifiers::*;
+
+        match modifier {
+            AltLeft(m) => self.alt.left = m,
+            AltRight(m) => self.alt.right = m,
+            CapsLock => self.caps_lock = !self.caps_lock,
+            ControlLeft(m) => self.control.left = m,
+            ControlRight(m) => self.control.right = m,
+            NumLock => self.num_lock = !self.num_lock,
+            ScrollLock => self.num_lock = !self.scroll_lock,
+            ShiftLeft(m) => self.shift.left = m,
+            ShiftRight(m) => self.shift.right = m,
+        }
+    }
+}
+
+/// Possible types of keyboard input we might receive,
+pub enum Key {
+    Ascii(u8),
+    Meta(Modifiers),
+    LowerAscii(u8),
+}
+
+pub enum KeyEvent {
+    Pressed(Key),
+    Released(Key),
+}
+
+/// Read bytes until end of sequence and combine into a number.
+fn retrieve_bytes(scancode: u8) -> u64 {
+    let mut byte_sequence: Vec<u8> = vec![scancode];
+
+    // These scancodes are special - they indicate the start of a byte sequence which is sent when
+    // some keys are pressed. If they are the byte we receive, read until the end of the sequence.
+    if scancode == 0xE0 || scancode == 0xE1 {
+        // Read another byte from the keyboard.
+        let check: u8 = ps2_8042::read_char(scancode);
+
+        //TODO: Check if this is a special key, i.e 0xE0 + 0x1C = keypad enter.
+    }
+
+
+    byte_sequence
+        .iter()
+        .rev()
+        .fold(0, |acc, &b| (acc << 1) + b as u64)
+}
