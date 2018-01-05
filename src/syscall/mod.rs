b8da4548c97910b97e7b08b4e38699457891ab8e
too-r <william@huddie.net>
2018-01-05 18:23:34 +0000

    Add match_syscall function to check syscall numbers

--- src/syscall/mod.rs
+++ src/syscall/mod.rs
@@ -1,3 +1,20 @@
 pub mod process;
 
 pub use self::process::*;
+
+#[inline(never)]
+pub fn match_syscall(num: u64, a: u64, b: u64, c: u64, d: u64, e: u64, f: u64) -> u64 {
+    match num {
+        // sys_execve
+        11 => {
+            let res = match process::create(test_syscall_proc, "test_syscall_proc") {
+                // Success.
+                Ok(_) => 0,
+                // Failure - some error code.
+                Err(e) => e,
+            };
+            
+            res
+        }
+    }
+}
