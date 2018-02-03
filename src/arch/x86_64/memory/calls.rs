885c796698a9c8768cd145c8ef95c1e798aef835
too-r <william@huddie.net>
2018-02-03 10:09:13 +0000

    Remove grant struct

--- src/arch/x86_64/memory/calls.rs
+++ src/arch/x86_64/memory/calls.rs
@@ -4,41 +4,27 @@ use super::paging::ActivePageTable;
 use super::{Frame, FrameAllocator};
 use super::paging::Page;
 
-/// A bit of memory granted to a userpace process.
-pub struct Grant {
-    start: VirtualAddress,
-    size: usize,
-    flags: EntryFlags,
-    is_mapped: bool,
-}
-
-impl Grant {
-    pub unsafe fn physmap(from: PhysicalAddress, to: VirtualAddress, flags: EntryFlags, size: usize) -> Grant {
-        let mut active_table = ActivePageTable::new();
-        let allocator = super::allocator();
-
-        let start_page = Page::containing_address(to);
-        let end_page = Page::containing_address(to + size - 1);
-        
-        // Map all pages in the calculated range.
-        for page in Page::range_inclusive(start_page, end_page) {
-            // Knowing that this frame already exists, we can calulate it.
-            let frame = Frame::containing_address((page.start_address() - to + from ) as PhysicalAddress);
-            active_table.map_to(page, frame, flags, allocator);
-        }
-
-        Grant {
-            start: to,
-            size: size,
-            flags: flags,
-            is_mapped: true,
-        }
-    }
-}
-
 /// Allocate some physical memory.
 pub fn physalloc(size: usize) -> Result<usize, &'static str> {
     let allocator = unsafe { super::allocator() };
 
     allocator.allocate_frame((size + 4095) / 4096).ok_or("OOM").map(|frame| frame.start_address())
+}
+
+/// Map the physical frames to virtual memory in the range `to + size`.
+pub unsafe fn physmap(from: PhysicalAddress, to: VirtualAddress, flags: EntryFlags, size: usize) {
+    let mut active_table = ActivePageTable::new();
+    let allocator = super::allocator();
+
+    let start_page = Page::containing_address(to);
+    let end_page = Page::containing_address(to + size - 1);
+        
+    // Map all pages in the calculated range.
+    for page in Page::range_inclusive(start_page, end_page) {
+        // Knowing that this frame already exists, we can calulate it.
+        let frame = Frame::containing_address((page.start_address() - to + from ) as PhysicalAddress);
+        active_table.map_to(page, frame, flags, allocator);
+    }
+
+    // TODO: Zero the mapped memory.
 }
