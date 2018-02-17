b593197ba701280b17cfa1bb8ba568dc058385ad
too-r <william@huddie.net>
2018-02-17 11:16:30 +0000

    Add SDT header

--- src/acpi/sdt.rs
+++ src/acpi/sdt.rs
@@ -1,4 +1,29 @@
-#[derive(Copy, Clone, Debug)]
-struct SdtHeader {
+use core::mem;
 
+#[derive(Copy, Clone, Debug)]
+pub struct SdtHeader {
+    signature: [u8; 4],
+    length: u32,
+    revision: u8,
+    checksum: u8,
+    oem_id: [u8; 6],
+    oem_table_id: [u8; 8],
+    oem_revision: u32,
+    creator_id: u32,
+    creator_rev: u32,
+}
+
+impl SdtHeader {
+    /// Check if this is valid.
+    pub fn checksum(&self) -> bool {
+        let mut sum: u8 = 0;
+
+        for i in 0..self.length {
+            let slice: [u8; mem::size_of::<Self>()] = unsafe { mem::transmute_copy(self) };
+
+            sum = slice.iter().sum();
+        }
+
+        sum == 0
+    }
 }
