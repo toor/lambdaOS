8739c8c206307e85c1c4a50b3c4f2d2b06dccd0f
too-r <william@huddie.net>
2018-02-24 14:11:56 +0000

    Add utility methods on SdtHeader

--- src/acpi/sdt.rs
+++ src/acpi/sdt.rs
@@ -1,4 +1,5 @@
 use core::mem;
+use core::slice;
 
 #[derive(Copy, Clone, Debug)]
 pub struct SdtHeader {
@@ -25,5 +26,28 @@ impl SdtHeader {
         }
 
         sum == 0
+    }
+    
+    /// Return the starting address of this table's data.
+    pub fn data_address(&self) -> usize {
+        self as *const _ as usize + mem::size_of::<Self>()
+    }
+
+    /// Return the length of this table's data.
+    pub fn data_len(&self) -> usize {
+        let total_size = self.length as usize;
+        let header_size = mem::size_of::<Self>();
+        
+        // Check if the length is bigger than the header itself. If it is, other data exists.
+        if total_size >= header_size {
+            return total_size - header_size;
+        } else {
+            0 // No extra data.
+        }
+    }
+
+    /// Return a slice of this table's data.
+    pub unsafe fn data(&self) -> &[u8] {
+        slice::from_raw_parts(self.data_address() as *const u8, self.data_len())
     }
 }
